12f27479e05152401497b3863be349bba2cfe69f
Edward Bridges <github@eqbridges.com>
2017-07-01 05:00:23 -0400

    add class to simply model a daterange

---
+++ transaction_downloader/daterange.py
@@ -0,0 +1,22 @@
+from dateutil.parser import parse
+
+class Daterange(object):
+  def __init__(self, start, end):
+    self.start = start
+    self.end = end
+
+  @staticmethod
+  def format_date(date):
+    d = parse(date)
+    return d.strftime('%Y-%m-%d')
+
+  def start(self):
+    return self.start
+
+  def end(self):
+    return self.end
+
+  def as_filename(self, account, fmt):
+    fmt_start = format_date(self.start)
+    fmt_end = format_date(self.end)
+    return '%s--%s-%s.%s' % (fmt_start, fmt_end, account, fmt)
