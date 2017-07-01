1a87e6cb3a04e1157c26f63cd693705628afad00
Edward Bridges <github@eqbridges.com>
2017-07-01 07:37:07 -0400

    define a customized output handle for writing to

--- transaction_downloader/transaction_writer.py
+++ transaction_downloader/transaction_writer.py
@@ -4,14 +4,15 @@ from decimal import Decimal
 TWOPLACES = Decimal(10) ** -2
 
 class TransactionWriter(object):
-  def __init__(self):
-    pass
+  def __init__(self, output):
+    if output:
+      self.output = output
 
-  def instance(t):
+  def instance(t, output):
     if t == 'csv':
-      return CsvTransactionWriter()
+      return CsvTransactionWriter(output)
     if t == 'qif':
-      return QifTransactionWriter()
+      return QifTransactionWriter(output)
 
   instance = staticmethod(instance)
 
@@ -26,31 +27,31 @@ class TransactionWriter(object):
 
 class CsvTransactionWriter(TransactionWriter):
   def begin(self, account_info):
-    print('Date,Amount,Description')
+    print('Date,Amount,Description', file=self.output)
 
   def write_record(self, transaction):
-    print(transaction['date'],transaction['amount'],transaction['name'])
+    print(transaction['date'],transaction['amount'],transaction['name'], file=self.output)
 
 
 
 class QifTransactionWriter(TransactionWriter):
   def begin(self, info):
-    print('!Account')
-    print('N%s' % info['account-path'])
-    print('T%s' % info['account-type'])
+    print('!Account', file=self.output)
+    print('N%s' % info['account-path'], file=self.output)
+    print('T%s' % info['account-type'], file=self.output)
     if 'account_description' in info:
-      print('D%s' % info['account_description'])
-    print('^')
-    print('!Type:%s' % info['account-type'])
+      print('D%s' % info['account_description'], file=self.output)
+    print('^', file=self.output)
+    print('!Type:%s' % info['account-type'], file=self.output)
 
 
   def write_record(self, transaction):
-    print('C') # cleared status: Values are blank (not cleared), "*" or "c" (cleared) and "X" or "R" (reconciled).
-    print('D%s' % self.format_date(transaction['date']))
-    print('N%s' % self.format_chknum(transaction))
-    print('P%s' % transaction['name'])
-    print('T%s' % self.format_amount(transaction['amount']))
-    print('^')
+    print('C', file=self.output) # cleared status: Values are blank (not cleared), "*" or "c" (cleared) and "X" or "R" (reconciled).
+    print('D%s' % self.format_date(transaction['date']), file=self.output)
+    print('N%s' % self.format_chknum(transaction), file=self.output)
+    print('P%s' % transaction['name'], file=self.output)
+    print('T%s' % self.format_amount(transaction['amount']), file=self.output)
+    print('^', file=self.output)
 
 
   def format_date(self, date):
