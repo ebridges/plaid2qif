d044fa982389235e8d1e2f8271567b1c901d1be5
Edward Q. Bridges <ebridges@roja.cc>
2020-01-20 13:16:37 -0500

    add release script

--- release.sh
+++ release.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 VERSION=${1}
+DEPLOY=${2}
 
 if [ -z "${VERSION}" ];
 then
-    echo "Usage: ${0} <version>"
+    echo "Usage: ${0} <version> [--deploy]"
     exit 1
 fi
 
+echo "VERSION='${VERSION}'" > plaid2qif/__init__.py
 git tag --sign v${VERSION} --message="Release v${VERSION}"
 git push origin --tags
+
+if [ ! -z "${DEPLOY}" ];
+then
+    python3 setup.py sdist bdist_wheel
+    twine upload dist/*
+fi
