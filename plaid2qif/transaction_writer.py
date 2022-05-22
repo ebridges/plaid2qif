6d3a224f5c29e8477cfe91817ef687d596bdf12f
Edward Bridges <github@eqbridges.com>
2022-05-22 19:43:01 -0400

    #20 dates & json formatting are now handled differently by the underlying library.  cf.: https://github.com/plaid/plaid-python\#data-type-differences-from-api-and-from-previous-versions

--- plaid2qif/transaction_writer.py
+++ plaid2qif/transaction_writer.py
@@ -2,6 +2,7 @@ from dateutil.parser import parse
 from decimal import Decimal
 from logging import info
 from json import dumps
+from datetime import datetime, date
 import unicodedata
 
 TWOPLACES = Decimal(10) ** -2
@@ -36,7 +37,10 @@ class JsonTransactionWriter(TransactionWriter):
     print( dumps(account_info, sort_keys=True), file=self.output)
 
   def write_record(self, transaction):
-    print( dumps(transaction, sort_keys=True), file=self.output)
+    def encode_date(obj):
+        if isinstance(obj, (date, datetime)):
+            return obj.isoformat()
+    print( dumps(transaction.to_dict(), sort_keys=True, default=encode_date), file=self.output)
 
 
 class CsvTransactionWriter(TransactionWriter):
@@ -80,8 +84,7 @@ class QifTransactionWriter(TransactionWriter):
 
 
   def format_date(self, date):
-    d = parse(date)
-    return d.strftime('%m/%d/%Y')
+    return date.strftime('%m/%d/%Y')
 
 
   def format_chknum(self, t):
